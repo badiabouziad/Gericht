d9baf166de3c42b704569f04d3479c9245dd7d01
SamiCheurfi <148457893+SamiCheurfi@users.noreply.github.com>
2024-05-25 10:48:14 +0100

    first files

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import AboutUs from "./container/AboutUs/AboutUs";
+import Header from "./container/Header/Header";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Chef from "./container/Chef/Chef.jsx";
+import SpecialMenu from "./container/Menu/SpecialMenu.jsx";
+import Intro from "./container/Intro/Intro.jsx";
+import Laurels from "./container/Laurels/Laurels.jsx";
+import Gallery from "./container/Gallery/Gallery.jsx";
+import FindUs from "./container/Findus/FindUs.jsx";
+import Footer from "./container/Footer/Footer.jsx";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
